8ea8073c8c6d248d0f7f58ec27bde16f158e47e8
InitSync <benjaalejandro206@gmail.com>
2022-10-30 10:56:42 -0300

    feat(loader): added XConfig manager and handler, and created configuration files

--- bukkit/src/main/java/net/xstream/Loader.java
+++ bukkit/src/main/java/net/xstream/Loader.java
@@ -1,5 +1,8 @@
 package net.xstream;
 
+import net.xconfig.config.ConfigurationHandler;
+import net.xconfig.config.ConfigurationModel;
+import net.xconfig.services.ConfigurationService;
 import net.xstream.api.AbstractLoader;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,17 +19,49 @@ import java.util.Objects;
 public final class Loader extends AbstractLoader {
 	private final XStream plugin;
 	
+	private ConfigurationModel configurationManager;
+	private ConfigurationHandler configurationHandler;
+	
 	public Loader(@NotNull XStream plugin) {
 		this.plugin = Objects.requireNonNull(plugin, "The XStream instance is null.");
 	}
 	
+	/**
+	 * Returns the ConfigurationModel object.
+	 *
+	 * @return A ConfigurationModel object.
+	 */
+	public @NotNull ConfigurationModel configurationManager() {
+		if (this.configurationManager == null) {
+			throw new IllegalStateException("Cannot access to the ConfigurationModel object.");
+		}
+		return this.configurationManager;
+	}
+	
+	/**
+	 * Returns the ConfigurationHandler object, if it is null, will be throws an
+	 * IllegalStateException.
+	 *
+	 * @return A ConfigurationHandler object.
+	 */
+	public @NotNull ConfigurationHandler configurationHandler() {
+		if (this.configurationHandler == null) {
+			throw new IllegalStateException("Cannot access to the ConfigurationHandler instance.");
+		}
+		return this.configurationHandler;
+	}
+	
 	@Override
 	public void enable() {
-	
+		this.configurationManager = ConfigurationService.manager(this.plugin);
+		this.configurationManager.create("", "config.yml");
+		this.configurationManager.create("", "messages.yml");
+		this.configurationHandler = ConfigurationService.handler(this.configurationManager);
 	}
 	
 	@Override
 	public void disable() {
-	
+		if (this.configurationManager != null) this.configurationManager = null;
+		if (this.configurationHandler != null) this.configurationHandler = null;
 	}
 }
