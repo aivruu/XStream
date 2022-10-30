0c3d0e7e10b0ae8fc565c16c76615ec3a9d1d96a
InitSync <benjaalejandro206@gmail.com>
2022-10-30 12:03:42 -0300

    feat(bungee): added main class and configuration file to 'bungee' subproject

---
+++ bungee/src/main/java/net/xstream/XStream.java
@@ -0,0 +1,15 @@
+package net.xstream;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class XStream extends Plugin {
+	@Override
+	public void onEnable() {
+	
+	}
+	
+	@Override
+	public void onDisable() {
+	
+	}
+}
