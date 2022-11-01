f741e6abc42a1414702f4bfb8f52c0fefefd55bc
InitSync <benjaalejandro206@gmail.com>
2022-11-01 15:54:22 -0300

    feat(loader-service): implemented 'commandLoader()' method

--- bungee/src/main/java/net/xstream/bungee/services/LoaderService.java
+++ bungee/src/main/java/net/xstream/bungee/services/LoaderService.java
@@ -1,7 +1,9 @@
 package net.xstream.bungee.services;
 
+import net.md_5.bungee.api.plugin.Plugin;
 import net.xstream.bungee.Loader;
 import net.xstream.bungee.XStream;
+import net.xstream.bungee.loaders.CommandLoader;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,5 +24,16 @@ public interface LoaderService {
 	@Contract ("_ -> new")
 	static @NotNull Loader loader(@NotNull XStream plugin) {
 		return new Loader(plugin);
+	}
+	
+	/**
+	 * Returns a new CommandLoader.Builder object.
+	 *
+	 * @param plugin A Plugin instance.
+	 * @return A CommandLoader.Builder object
+	 */
+	@Contract (value = "_ -> new", pure = true)
+	static CommandLoader.@NotNull Builder commandLoader(@NotNull Plugin plugin) {
+		return new CommandLoader.Builder(plugin);
 	}
 }
