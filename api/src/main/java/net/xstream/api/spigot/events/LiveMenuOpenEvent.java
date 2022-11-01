48a4809f7a4a858547719d5e728b00dd53cc1dc5
InitSync <benjaalejandro206@gmail.com>
2022-11-01 10:15:36 -0300

    feat(events): implemented custom events of the API for Bukkit platforms

--- api/src/main/java/net/xstream/api/spigot/events/LiveMenuOpenEvent.java
+++ api/src/main/java/net/xstream/api/spigot/events/LiveMenuOpenEvent.java
@@ -1,12 +1,15 @@
-package net.xstream.api.events;
+package net.xstream.api.spigot.events;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 /**
- * Event for Bukkit servers that handles when a stream is announced.
+ * Event for Bukkit Servers called when the player opens the Live Manager Menu.
  *
  * @author InitSync
  * @version 1.0.0
@@ -14,20 +17,21 @@ import org.jetbrains.annotations.NotNull;
  * @see org.bukkit.event.Event
  * @see org.bukkit.event.Cancellable
  */
-public final class StreamAnnounceEvent extends Event implements Cancellable {
+public final class LiveMenuOpenEvent extends Event implements Cancellable {
 	private final HandlerList handlers;
+	private final Player player;
 	
 	private boolean cancelled;
 	
-	public StreamAnnounceEvent() {
+	public LiveMenuOpenEvent(@NotNull Player player) {
 		this.handlers = new HandlerList();
+		this.player = Objects.requireNonNull(player, "The player is null.");
 	}
 	
-	@Override
-	public @NotNull HandlerList getHandlers() {
-		return this.handlers;
+	public @NotNull Player player() {
+		return this.player;
 	}
-	
+
 	@Override
 	public boolean isCancelled() {
 		return this.cancelled;
@@ -36,5 +40,10 @@ public final class StreamAnnounceEvent extends Event implements Cancellable {
 	@Override
 	public void setCancelled(boolean cancel) {
 		this.cancelled = cancel;
+	}
+	
+	@Override
+	public @NotNull HandlerList getHandlers() {
+		return this.handlers;
 	}
 }
---
+++ api/src/main/java/net/xstream/api/spigot/events/StreamPrepareEvent.java
@@ -0,0 +1,49 @@
+package net.xstream.api.spigot.events;
+
+import org.apache.commons.lang.Validate;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Bukkit Event called when the streamer sets the link of stream.
+ *
+ * @author InitSync
+ * @version 1.0.0
+ * @since 1.0.0
+ * @see org.bukkit.event.Event
+ * @see org.bukkit.event.Cancellable
+ */
+public final class StreamPrepareEvent extends Event implements Cancellable {
+	private final HandlerList handlers;
+	private final String link;
+	
+	private boolean cancelled;
+	
+	public StreamPrepareEvent(@NotNull String link) {
+		this.handlers = new HandlerList();
+		
+		Validate.notEmpty(link, "The link is empty.");
+		this.link = link;
+	}
+	
+	public @NotNull String getLink() {
+		return this.link;
+	}
+	
+	@Override
+	public boolean isCancelled() {
+		return this.cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancel) {
+		this.cancelled = cancel;
+	}
+	
+	@Override
+	public @NotNull HandlerList getHandlers() {
+		return this.handlers;
+	}
+}
