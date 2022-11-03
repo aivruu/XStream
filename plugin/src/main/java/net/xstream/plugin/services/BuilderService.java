1ba73639e55f756f07607b0fcf5c87120954def6
InitSync <benjaalejandro206@gmail.com>
2022-11-03 17:17:42 -0300

    refactor(builder-service): modified parameter type of 'fromMaterial()' method and added @Contract annotation

--- plugin/src/main/java/net/xstream/plugin/services/BuilderService.java
+++ plugin/src/main/java/net/xstream/plugin/services/BuilderService.java
@@ -2,10 +2,21 @@ package net.xstream.plugin.services;
 
 import com.cryptomorin.xseries.XMaterial;
 import net.xstream.plugin.builders.ItemBuilder;
+import org.apache.commons.lang.Validate;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public interface BuilderService {
-	static ItemBuilder.Builder fromMaterial(@NotNull XMaterial material) {
-		return new ItemBuilder.Builder(material.parseMaterial());
+	/**
+	 * Returns a new ItemBuilder.Builder object.
+	 *
+	 * @param materialName Name of material to match.
+	 * @return A ItemBuilder.Builder instance.
+	 */
+	@Contract ("_ -> new")
+	static ItemBuilder.@NotNull Builder fromMaterial(@NotNull String materialName) {
+		Validate.notEmpty(materialName, "The material name is empty.");
+		
+		return new ItemBuilder.Builder(XMaterial.matchXMaterial(materialName).get().parseMaterial());
 	}
 }
